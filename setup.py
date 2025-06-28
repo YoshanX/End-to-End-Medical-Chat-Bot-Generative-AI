5b39cf9a9cf1933cabc8e48b9e43cdd0850f3817
YoshanX <s19840@sci.pdn.ac.lk>
2025-06-28 20:50:39 +0530

    folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Yoshan Pramuditha',
+    author_email= 'yoshanpramuditha@gamail.com@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
